d530d11cea1beeb79c3f8c8be4517b797704d4dd
Chris Klanac <cklanac@users.noreply.github.com>
2018-06-29 13:25:16 -0400

    initial commit

---
+++ public/scripts/index.js
@@ -0,0 +1,7 @@
+/* global $ noteful */
+'use strict';
+
+$(document).ready(function () {
+  noteful.bindEventListeners();
+  noteful.doSearchAndRender();
+});
---
+++ server.js
@@ -0,0 +1,49 @@
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+
+const { PORT } = require('./config');
+const notesRouter = require('./routes/notes.router');
+
+// Create an Express application
+const app = express();
+
+// Log all requests
+app.use(morgan('dev'));
+
+// Create a static webserver
+app.use(express.static('public'));
+
+// Parse request body
+app.use(express.json());
+
+// Mount router on "/api"
+app.use('/api', notesRouter);
+
+// Catch-all 404
+app.use(function (req, res, next) {
+  const err = new Error('Not Found');
+  err.status = 404;
+  next(err);
+});
+
+// Catch-all Error handler
+app.use(function (err, req, res, next) {
+  res.status(err.status || 500);
+  res.json({
+    message: err.message,
+    error: app.get('env') === 'development' ? err : {}
+  });
+});
+
+// Listen for incoming connections
+if (require.main === module) {
+  app.listen(PORT, function () {
+    console.info(`Server listening on ${this.address().port}`);
+  }).on('error', err => {
+    console.error(err);
+  });
+}
+
+module.exports = app; // Export for testing
